313a46434a890dfc80a51fc4d44b9fe6dcf07aaf
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-03-31 14:24:44 -0700

    estilo mejorado

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,39 @@
 
+// salidas
+const salida = document.getElementById("resultado")
+
+// inputs
+const minimo = document.getElementById("minimo")
+const maximo = document.getElementById("maximo")
+const periodo = document.getElementById("periodo")
+const interes = document.getElementById("interes");
+
+// divs
+const flujosEfectivo = document.getElementById("entradas")
+
+
+
+
+
+/**
+ * Copiar un valor de una entrada a otra
+ */
 function copyValue(a,b) {
     let foo = document.getElementById(a).value;
     document.getElementById(b).value = foo;
 }
 
+
+
+
+
 /**
  * Generar los campos para ingresar el flujo de efectivo
  */
 function generarEntradas() {
-    const min = parseInt(document.getElementById("minimo").value);
-    const max = parseInt(document.getElementById("maximo").value);
-    const entradas = document.getElementById("entradas");
+    const min = parseInt(minimo.value);
+    const max = parseInt(maximo.value);
+    const entradas = flujosEfectivo;
 
     // Limpiar las entradas existentes
     entradas.innerHTML = "";
@@ -30,35 +53,38 @@ function generarEntradas() {
     }
 }
 
+
+
+
+
+
 /**
  * Obtener los valores del arreglo
  * @return array
  */
 function obtenerValores() {
-    var entradas = Array.from(document.querySelectorAll("#entradas input"));
+    const entradas = Array.from(document.querySelectorAll("#entradas input"));
     return entradas.map(entrada => parseFloat(eval(entrada.value)) || 0);
 }
+
 
 /**
  * vacia el formulario y borras las entradas
  */
 function limpiarFormulario() {
-    var minimo = document.getElementById("minimo");
-    var maximo = document.getElementById("maximo");
-    var periodo = document.getElementById("periodo");
-    var interes = document.getElementById("interes");
-    var resultado = document.getElementById("resultado");
-
     // Limpiar las entradas existentes
-    entradas.innerHTML = "";
+    flujosEfectivo.innerHTML = "";
 
     // Vaciar los campos de rango mínimo y máximo
     minimo.value = "";
     maximo.value = "";
     periodo.value = "";
     interes.value = "";
-    resultado.value = "";
+    salida.value = "";
 }
+
+
+
 
 
 /**
@@ -68,18 +94,22 @@ function financial(x) {
   return Number.parseFloat(x).toFixed(2);
 }
 
+
+
+
+
 /**
  * Calcula la quivalencia en el periodo
  */
 function calculadoradeequivalencia() {
-    const periodoinicio = parseInt(document.getElementById("minimo").value);
-    const periodofinal = parseInt(document.getElementById("maximo").value);
-    const periododeinteres = document.getElementById("periodo").value == "" ? periodoinicio : parseInt(document.getElementById("periodo").value);
-    const interes = parseFloat(document.getElementById("interes").value);
+    const periodoinicio = parseInt(minimo.value);
+    const periodofinal = parseInt(maximo.value);
+    const periododeinteres = periodo.value == "" ? periodoinicio : parseInt(periodo.value);
+    const interesPorcentual = parseFloat(interes.value);
     
     const flujo = obtenerValores();
     const nflujos = Math.abs(periodoinicio - periodofinal);
-    const porcentual = interes / 100;
+    const porcentual = interesPorcentual / 100;
     const pinteres = Math.abs(periodoinicio - periododeinteres);
     
     document.getElementById("resultado").value = financial(flujonv(flujo, nflujos, porcentual, pinteres));
